9c4a2c5e0663f9a681cb1d4bfa78dff9a6da6c39
Gabriel <gabrielturing@gmail.com>
2020-08-13 18:43:53 -0300

    Inicializacao

---
+++ code.php
@@ -0,0 +1,52 @@
+<?php
+    function getMunicipios()
+    {
+        $url = 'http://extranet.artesp.sp.gov.br/origemDestino/include/asp/combos.asp?municipios=1&string=';
+        $data = file_get_contents($url);
+        return trim($data);
+    }
+
+    function retirarLixo($data)
+    {
+        $lixo = [
+            '<?xml version=\'1.0\' encoding=\'ISO-8859-1\'?>', '<Combos>', '</Combos>', '<Secoes>', '</Secoes>', '<Secao data="'
+        ];        
+
+        foreach($lixo as $value){
+            $data = str_replace($value, '', $data);
+        }
+
+        $enter = [
+            ";" => '"/>', ',' => 'label="'
+        ];
+
+        foreach($enter as $key => $value){
+            $data = str_replace($value, $key, $data);
+        }
+
+        $data = str_replace('"', '', $data);
+        $data = str_replace(' ', '', $data);
+
+        return $data;
+    }
+
+    $ret = retirarLixo(getMunicipios());
+
+    $ret = explode(';', $ret);
+    
+    array_pop($ret);
+
+    $array;
+    foreach($ret as $value){
+        $tmp = explode(',', $value);
+        $array[$tmp[0]] = $tmp[1];
+    }
+
+    print_r($array);
+
+    /*
+
+    for($i = 0, $j = 1; $i < (count($test)); $i += 2, $j += 2){
+        echo $test[$i].'-'.$test[$j];
+    }*/
+?>
